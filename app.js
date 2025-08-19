2fc2cb3edafe749eaebb711ae8ca11da1f788bbf
joelru_ <juanjoeln8@gmail.com>
2025-08-19 15:22:33 -0500

    02_Crear_un_array_para_almacenar_los_nombres
    * Se añade array para guardar nombres agregados
    * Se añade función agregarAmigo
    * Se alade función mostrarLista

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+//funcion para agregar amigos
+function agregarAmigo() {
+ 
+}
+
+//funcion para mostrar la lista de amigos agregados
+function mostrarLista() {
+ 
+}
+
